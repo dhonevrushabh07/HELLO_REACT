66cc96758a0dfdbcca79988ef4cb36a0fa169a8a
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 00:17:53 +0530

    b9 app.js updated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useState } from "react";
+
 export default function App() {
-  let counter = 100;
-  let username = "Vrushabh";
-  let email = "vrushabh@gmail.com";
+  let [counter, setCounter] = useState(100);
+
+  let Increment = () => setCounter(counter + 1);
+  let Decrement = () => setCounter(counter - 1);
 
   return (
     <div>
       <div>{counter}</div>
-      <div>{username}</div>
-      <div>{email}</div>
+      <input type="button" value="Increment" onClick={Increment}></input>
+      <input type="button" value="Decrement" onClick={Decrement}></input>
     </div>
   );
 }
