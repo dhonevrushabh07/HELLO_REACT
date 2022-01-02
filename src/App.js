e5a4cbb02f7e12c2f679d472ee81c96510889639
dhonevrushabh <dhonevrushabh07@gmail.com>
2022-01-03 00:04:22 +0530

    b7 aap.js updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 const App = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello World</h1>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad officia
+        corporis mollitia quas fugit quibusdam minima similique assumenda labore
+        necessitatibus, architecto aliquid consequatur rem, deserunt quis
+        provident modi, molestias natus.
+      </div>
+
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad officia
+        corporis mollitia quas fugit quibusdam minima similique assumenda labore
+        necessitatibus, architecto aliquid consequatur rem, deserunt quis
+        provident modi, molestias natus.
+      </div>
     </div>
   );
 };
